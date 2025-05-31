c3672e557249d626f970822d9c87f4cada5f6188
Sanju <55499705+nevatia@users.noreply.github.com>
2025-05-31 10:48:01 +0530

    extension loading during run , disabled

--- sanju_Tatkal_booking.py
+++ sanju_Tatkal_booking.py
@@ -10,6 +10,9 @@ import time
 from selenium.common.exceptions import NoAlertPresentException
 import json
 import sys
+options = uc.ChromeOptions()
+options.add_argument("--disable-extensions")
+driver = uc.Chrome(options=options)
 
 #Load data
 passengers  = 'passenger_data.json'
